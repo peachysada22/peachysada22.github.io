5ad6c3e0d6e7f23ffd35e6f0353e9fac8605e13c
Sarahi Hernandez <peachysada22@gmail.com>
2017-02-15 20:00:17 +0000

    Adding Animation

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -2,21 +2,49 @@ var NUM_CIRCLES = 12;
 var circleDiameter;
 var circleRadius;
 
+var rVal;
+var gVal;
+var bVal;
+
 function setup() {
     createCanvas(480, 600);
+    
+    frameRate(5);
+    
     circleDiameter = width/NUM_CIRCLES;
     circleRadius = circleDiameter/2;
+    
+    rVal = 255;
+    gVal = 0;
+    bVal = 0;
 }
+
 function draw() {
-    var y = 0;
-    while (y <= height) {
+    var isShifted = false;
     
-        var x = 0;
-        while (x <= width) {
-            ellipse(x, y, circleDiameter, circleDiameter);  
-            x = x + circleDiameter;
+    var y = height;
+    while (y >= 0) {
+        
+        var x;
+        
+        if (isShifted) {
+         x = circleRadius;
+    } else {
+        x= 0;
+    }
+        
+    while (x <= width) {
+        stroke(color(rVal,gVal,bVal));
+        fill(color(rVal,gVal,bVal));
+        ellipse(x, y, circleDiameter, circleDiameter);
+         x = x + circleDiameter;
         }
-    
-    y = y + circleRadius;
-  }
+        
+        y = y - circleRadius
+        isShifted = !isShifted;
+        
+        rVal= (rVal + 254) % 256;
+        gVal = (gVal+ 7) % 256;
+        bVal = (bVal + 3) % 256;
+    }
 }
