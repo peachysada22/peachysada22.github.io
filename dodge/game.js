86f6864560b3733811a5432e895d690aa9847d63
Sarahi Hernandez <peachysada22@gmail.com>
2017-02-23 19:52:46 +0000

    corrections

--- dodge/game.js
+++ dodge/game.js
@@ -5,17 +5,24 @@ var enemy;
 var enemyImage;
 var backgroundImage;
 
+function preload() {
+  playerImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/N5uCbDu.png");
+  enemyImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/OdL0XPt.png");
+  backgroundImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/aKQOg3G.png");
+}
 
 function setup() {
-    isGameOver = false;
-    createCanvas(256, 256);
-    player = createSprite(width/2, height-25, 50, 50);
-    enemy = createSprite(width / 2, 0, 0, 0);
-    enemy.rotationSpeed = 4.0;
-}    
+  isGameOver = false;
+  createCanvas(256, 256);
+  player = createSprite(width / 2, height - (playerImage.height / 2), 0, 0);
+  player.addImage(playerImage);
+  enemy = createSprite(width / 2, 0, 0, 0);
+  enemy.addImage(enemyImage);
+  enemy.rotationSpeed = 4.0;
+}
 
 function draw() {
-    if (isGameOver) {
+  if (isGameOver) {
     gameOver();
   } else {
     if (enemy.overlap(player)) {
@@ -24,20 +31,20 @@ function draw() {
     if (enemy.overlap(player)) {
       gameOver();
     }
-    background(0, 0, 100);
-    if (keyDown(RIGHT_ARROW) && player.position.x < ((width / 2))) {
+    background(backgroundImage);
+    if (keyDown(RIGHT_ARROW) && player.position.x < (width - (playerImage.width / 2))) {
       player.position.x += 2;
     }
-    if (keyDown(LEFT_ARROW) && player.position.x > (width / 2)) {
+    if (keyDown(LEFT_ARROW) && player.position.x > (playerImage.width / 2)) {
       player.position.x -= 2;
     }
     enemy.position.y = enemy.position.y + 3;
     if (enemy.position.y > height) {
       enemy.position.y = 0;
       enemy.position.x = random(5, width - 5);
-     }
-     drawSprites();
     }
+    drawSprites();
+  }
 }
 
 function gameOver() {
@@ -51,7 +58,7 @@ function gameOver() {
 function mouseClicked() {
   isGameOver = false;
   player.position.x = width / 2;
-  player.position.y = height - (height / 2);
+  player.position.y = height - (playerImage.height / 2);
   enemy.position.x = width / 2;
   enemy.position.y = 0;
 }
